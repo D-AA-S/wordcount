3d8ec6ebec7b8967e805338c7173fbf719adf113
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2021-02-13 14:38:06 -0800

    Basic file checker added

--- wordcount/wordcount.cpp
+++ wordcount/wordcount.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
+#include <fstream> 
 
-void* progress_monitor(void*)
+/*void* progress_monitor(void*)
 {
 
-}
+}*/
 
-long wordcount(std::string fileName)
+long wordcount(const char* fileName)
 {
-
+    long ah = 1; 
+    std::ifstream check (fileName, std::ifstream::in);
+    if (!check.is_open()) { std::cout << "shit" << std::endl; }
+    else { std::cout << "pog" << std::endl; }
+    return ah; 
 }
 
 int main(int argc, char** argv)
 {
-
+    wordcount(argv[1]);
 }
 
 
